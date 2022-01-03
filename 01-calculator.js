070e276017a41041821676fc68eb56c586a48c65
NarGhCat <narr204@gmail.com>
2022-01-03 14:05:23 +0400

    function calculator

---
+++ 01-calculator.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// function Calculator() {
+  
+// }
+
+let animal = {
+    eats: true
+};
+  
+ let rabbit = {
+    name: 'roger',
+    __proto__: animal
+ };
+  
+
+console.log(rabbit.eats);
